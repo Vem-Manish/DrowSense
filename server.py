3a968b0a2c599b1a08097023b1f4c929b0487898
Manish Reddy <vemmanishreddy5@gmail.com>
2026-05-26 11:26:24 +0530

    Updated port for huggingface

--- server.py
+++ server.py
@@ -310,5 +310,5 @@ async def websocket_endpoint(ws: WebSocket):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 7860))
     uvicorn.run("server:app", host="0.0.0.0", port=port)
